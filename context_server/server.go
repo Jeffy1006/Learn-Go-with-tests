408c82b155bfde3abe4133bf72a40b5fc433e152
Kong Chan <jeff.kongchan@gmail.com>
2025-05-27 18:41:57 -0500

    Added Server (Context)

---
+++ context_server/server.go
@@ -0,0 +1,25 @@
+package contextserver
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func Server(store Store) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		data, err := store.Fetch(r.Context())
+
+		if err != nil {
+			log.Println("request got cancelled")
+			return
+		}
+
+		fmt.Fprint(w, data)
+	}
+}
+
+type Store interface {
+	Fetch(ctx context.Context) (string, error)
+}
